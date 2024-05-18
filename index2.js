97b237fa9c1061aa3eb2eeb213a1a77c1086aefa
ranahaamza00 <ranahaamza00@gmail.com>
2024-05-18 18:13:55 +0500

    first commit

---
+++ index2.js
@@ -0,0 +1,7 @@
+function city_name(city, country) {
+    if (country === void 0) { country = "pakistan"; }
+    console.log("".concat(city, " is in ").concat(country));
+}
+city_name("lahore");
+city_name("faisalabad");
+city_name("tokyo", "japan");
---
+++ index2.ts
@@ -0,0 +1,6 @@
+function city_name(city : string , country : string = "pakistan"){
+    console.log(`${city} is in ${country}`)
+} 
+city_name("lahore");
+city_name("faisalabad");
+city_name("tokyo" , "japan");
